bd9eadcc96783e76f2f3aafb82c2ecfbccc8d580
Dimitris Tetepoulidis <tetepoulidis@ieee.org>
2024-01-22 13:46:44 +0200

    remade main project + ssh key

---
+++ WebApi/WebApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+
+    public WeatherForecastController()
+    {
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public string Get()
+    {
+        return "Hello Campagnero";
+    }
+}
